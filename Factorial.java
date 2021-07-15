0e7490522d823dcc40709b733acc5a546ddcf90b
Saranya1811 <87498094+Saranya1811@users.noreply.github.com>
2021-07-16 01:51:17 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,19 @@
+package newproject1;
+
+public class Factorial {
+
+	  int factorial(int n)
+	  {
+	  
+	    return (n == 1 || n == 0) ? 1 : n * factorial(n - 1);
+	  
+	  }
+	  
+	  public static void main(String args[])     
+	  {
+	    Factorial obj = new Factorial();
+	    int num = 5;
+	    System.out.println("Factorial of " + num + 
+	                      " is " + obj.factorial(num));
+	   }
+	}
